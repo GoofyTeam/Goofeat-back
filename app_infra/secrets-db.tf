5a0d13584c4f5938512940104d043cb71578b30b
agahakan <qirahakan@gmail.com>
2025-09-06 21:30:08 +0200

    l

--- app_infra/secrets-db.tf
+++ app_infra/secrets-db.tf
@@ -7,7 +7,7 @@ resource "aws_secretsmanager_secret" "nestjs_db" {
 resource "aws_secretsmanager_secret_version" "nestjs_db_version" {
   secret_id = aws_secretsmanager_secret.nestjs_db.id
   secret_string = jsonencode({
-    DB_HOST     = var.db_host
+    DB_HOST     = aws_db_instance.goofeat_postgres.address
     DB_PORT     = tostring(var.db_port)
     DB_USERNAME = var.db_username
     DB_PASSWORD = var.db_password
